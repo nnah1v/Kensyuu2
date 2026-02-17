47f84ec733ddafe5ac115c3e047d6c4066f64365
Natsuka <nnah1v@gmail.com>
2026-02-17 09:59:16 +0900

    2026/2/17 624/1015

--- src/main/java/com/example/moattravel/entity/User.java
+++ src/main/java/com/example/moattravel/entity/User.java
@@ -20,7 +20,7 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
-	private Integer idInteger;
+	private Integer id;
 	
 	@Column(name = "name")
     private String name;
--- src/main/java/com/example/moattravel/service/UserService.java
+++ src/main/java/com/example/moattravel/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.moattravel.entity.Roles;
 import com.example.moattravel.entity.User;
 import com.example.moattravel.form.SignupForm;
+import com.example.moattravel.form.UserEditForm;
 import com.example.moattravel.repository.RoleRepository;
 import com.example.moattravel.repository.UserRepository;
 
@@ -39,9 +40,24 @@ public class UserService {
 		user.setEmail(signupForm.getEmail());
 		user.setPassword(passwordEncoder.encode(signupForm.getPassword()));
 		user.setRole(role);
-		user.setEnabled(true);
+		user.setEnabled(false);
 
 		return userRepository.save(user);
+	}
+
+	@Transactional
+	public void update(UserEditForm userEditForm) {
+		User user = userRepository.getReferenceById(userEditForm.getId());
+
+		user.setName(userEditForm.getName());
+		user.setFurigana(userEditForm.getFurigana());
+		user.setPostalCode(userEditForm.getPostalCode());
+		user.setAddress(userEditForm.getAddress());
+		user.setPhoneNumber(userEditForm.getPhoneNumber());
+		user.setEmail(userEditForm.getEmail());
+
+		userRepository.save(user);
+
 	}
 
 	//メールアドレスが登録済みかどうかをチェックする
@@ -50,10 +66,24 @@ public class UserService {
 
 		return user != null;
 	}
-	
+
 	//パスワードとパスワード（確認用）の入力値が一致するかどうかをチェックする
 	public boolean isSamePassword(String password, String passwordConfirmation) {
-		
+
 		return password.equals(passwordConfirmation);
+	}
+
+	//ユーザーを有効にする
+	@Transactional
+	public void enableUser(User user) {
+		user.setEnabled(true);
+		userRepository.save(user);
+	}
+
+	//メールアドレスが変更されたかどうかをチェックする
+	public boolean isEmailChanged(UserEditForm userEditForm) {
+		User currentUser = userRepository.getReferenceById(userEditForm.getId());
+
+		return !userEditForm.getEmail().equals(currentUser.getEmail());
 	}
 }
